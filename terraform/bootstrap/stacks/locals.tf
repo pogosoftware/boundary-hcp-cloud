21d06ee1e39a1222cb1f8c0e3ecd578ea33f15ad
Przemyslaw Pogorzelec <29803615+scrhicks@users.noreply.github.com>
2024-10-27 19:11:36 +0100

    Fixed project name

--- terraform/bootstrap/stacks/locals.tf
+++ terraform/bootstrap/stacks/locals.tf
@@ -1,6 +1,6 @@
 locals {
-  tfc_organization = data.hcp_organization.current.resource_name
-  tfc_project = data.hcp_project.current.resource_name
+  tfc_organization = data.hcp_organization.current.name
+  tfc_project = data.hcp_project.current.name
 
   name = "${local.tfc_project}-stacks-${var.environment}"
   sub_regex = "^organization:${local.tfc_organization}:project:${local.tfc_project}:stack:${var.tfc_stack}:.*"
