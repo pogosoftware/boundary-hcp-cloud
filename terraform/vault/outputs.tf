d2ac7040ec7adfb2937a83d60b1ef4b555f6622f
Przemyslaw Pogorzelec <29803615+scrhicks@users.noreply.github.com>
2024-10-28 12:41:52 +0100

    Removed outputs and terraform files

--- terraform/vault/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vault_apps_mount_name" {
-  description = "The name of Vault mount for apps"
-  value       = var.vault_apps_mount_name
-}
-
-output "vault_apps_boundary_secret_name" {
-  description = "The name of boundary secret in vault"
-  value       = var.vault_apps_boundary_secret_name
-}
