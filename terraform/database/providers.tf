d87563b0a7176fafae0b523ec81fc2d01e63d47d
Przemyslaw Pogorzelec <29803615+scrhicks@users.noreply.github.com>
2024-11-02 14:59:47 +0100

    Used db address insterd of endpoint

--- terraform/database/providers.tf
+++ terraform/database/providers.tf
@@ -1,5 +1,5 @@
 provider "postgresql" {
-  host            = module.postgres.db_instance_endpoint
+  host            = module.postgres.db_instance_address
   port            = module.postgres.db_instance_port
   database        = module.postgres.db_instance_name
   username        = module.postgres.db_instance_username
