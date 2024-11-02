35e8388a273d660fb571d4f9c9097b9fec6cb104
Przemyslaw Pogorzelec <29803615+scrhicks@users.noreply.github.com>
2024-11-02 16:31:29 +0100

    TMP change vault url to public

--- terraform/hcp-cloud/providers.tf
+++ terraform/hcp-cloud/providers.tf
@@ -1,4 +1,4 @@
 provider "vault" {
-  address = hcp_vault_cluster.this.vault_private_endpoint_url
+  address = hcp_vault_cluster.this.vault_public_endpoint_url
   token   = hcp_vault_cluster_admin_token.this.token
 }
